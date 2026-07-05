89497f41bd048ddb5adae751aa4e00dfe500fb9f
elisananda2020-cmd <elisananda2020@gmail.com>
2026-07-05 16:51:31 -0500

    Add files via upload

---
+++ BancoDeSangue/src/exception/DoadorNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package exception;
+
+
+public class DoadorNaoEncontradoException extends Exception {
+    public DoadorNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+}
---
+++ BancoDeSangue/src/model/StatusBolsa.java
@@ -0,0 +1,12 @@
+package model;
+
+public enum StatusBolsa {
+    COLETADA,
+    EM_ANALISE,
+    APROVADA,
+    DISPONIVEL,
+    RESERVADA,
+    UTILIZADA,
+    VENCIDA,
+    REPROVADA
+}
